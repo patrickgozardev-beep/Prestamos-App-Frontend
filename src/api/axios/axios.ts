9c600c3a2a6cfcb38aa6a2febcfd8479cc34bbcb
patrickgozardev-beep <patrick.gozar.dev@gmail.com>
2026-03-16 22:02:06 -0500

    modificando cosas

--- src/api/axios/axios.ts
+++ src/api/axios/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
-  //baseURL: import.meta.env.VITE_API_URL ||"https://prestamos-app-backend-production-71e2.up.railway.app/api",  
+  //baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL ||"https://prestamos-app-backend-production-71e2.up.railway.app/api",  
   withCredentials: true,
 });
 
