bacba0bd0295477c878cc6ca13d30b933c867812
patrickgozardev-beep <patrick.gozar.dev@gmail.com>
2026-03-16 23:18:45 -0500

    modificando cosas

--- src/pages/Prestamos/ReprogramarPrestamo.tsx
+++ src/pages/Prestamos/ReprogramarPrestamo.tsx
@@ -97,8 +97,11 @@ import CustomSelect from "../../components/CustomSelect";
                 label="" 
                 options={[
                   {value: "10", label: "10%"}, 
-                  {value: "20", label: "20%"}, 
+                  {value: "15", label: "15%"}, 
+                  {value: "20", label: "20%"},
+                  {value: "25", label: "25%"},
                   {value: "30", label: "30%"}
+
                 ]} 
                 value={interes}
                 onChange={(val) => setInteres(val)}
